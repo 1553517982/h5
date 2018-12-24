c22a943a94d26edd0a7e7b861a56127243f29585
1553517982@qq.com <1553517982@qq.com>
2018-12-24 12:04:38 +0800

    -m

--- msgCheck/msgCheck.py
+++ msgCheck/msgCheck.py
@@ -1,13 +1,19 @@
 #-*- coding:utf-8 -*-
 import ConfigParser
+import threading
 import sys
 import os
+from time import ctime,sleep
 import urllib2
 import json
+import re
+
 
 appId=""
 appSecret=""
 accessToken=""
+wordsDict={}
+wordsList=[]
 
 def getAccessToken():
     global appId
@@ -28,12 +34,46 @@ def checkMsgContent(msg):
     req = urllib2.Request(requrl, jdata)
     res_data = urllib2.urlopen(req)
     res = json.loads(res_data.read())
-    if(res["errcode"] != 0):
-        print msg.decode("utf-8").encode("gbk")
+    if (res["errcode"] != 0):
+        if(res["errcode"] == 87014):
+            print msg.decode("utf-8").encode("gbk")
+        else:
+            print res["errcode"]
+
+
+def findAllWords(filePath):
+    f = open(filePath, "r")
+    lines = f.readlines()
+    f.close()
+    length = len(lines)
+
+    if length == 0:
+        return
+    global wordsList
+    zhPattern = re.compile(u'[a-zA-Z0-9\u4e00-\u9fa5]+')
+    for line in lines:
+            line = unicode(line, 'utf-8')
+            results = re.findall(zhPattern, line)
+            for str in results:
+                if not wordsDict.has_key(str):
+                    wordsDict.setdefault(str,str)
+                    wordsList.append(str.encode('utf-8'))
+
+
+def findDirAllWords(path):
+    global wordsList
+    for file in os.listdir(path):
+        filepath = path + "//" + file
+        if os.path.isdir(filepath):
+            findDirAllWords(filepath)
+        elif os.path.isfile(filepath):
+            findAllWords(filepath)
+
 
 def checkWords():
     global appId
     global appSecret
+    global wordsList
     reload(sys)
     sys.setdefaultencoding('utf8')
     conf = ConfigParser.ConfigParser()
@@ -41,12 +81,28 @@ def checkWords():
     appId = conf.get("appConfig","appId")
     appSecret = conf.get("appConfig", "appSecret")
 
+    packFolders=[]
+    if (conf.has_section('checkDir')):
+        checkDirs = conf.items("checkDir")
+        for pairs in checkDirs:
+            findDirAllWords(pairs[1])
+
+    print("totalwords:"+ str(len(wordsList)))
+    print("load words success!")
     getAccessToken()
 
-    items = conf.items("words")
+    items=wordsList
+    threads = []
     if (len(items)>0):
             for item in items:
-                checkMsgContent(item[1])
-    os.system("pause")
+                t =threading.Thread(target=checkMsgContent,args=(item,))
+                threads.append(t)
+    if len(threads) > 0 :
+        for t in threads:
+            t.start()
+        t.join()
+
 
 checkWords()
+sleep(0.1)
+os.system("pause")
