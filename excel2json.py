54680e65cdc2532e007dc7df0a41312a4d3f0caa
1553517982@qq.com <1553517982@qq.com>
2019-04-19 17:22:39 +0800

    excel2json

--- excel2json.py
+++ excel2json.py
@@ -1,22 +1,47 @@
+import os
+import sys
+import re
 import xlrd
 from collections import OrderedDict
 import json
 import codecs
 
-wb = xlrd.open_workbook('file.xlsx')
 
-convert_list = []
-sh = wb.sheet_by_index(0)
-title = sh.row_values(0)
-for rownum in range(1, sh.nrows):
-    rowvalue = sh.row_values(rownum)
-    single = OrderedDict()
-    for colnum in range(0, len(rowvalue)):
-        print(title[colnum], rowvalue[colnum])
-        single[title[colnum]] = rowvalue[colnum]
-    convert_list.append(single)
+def checkExcelFile(filePath,fileName):
+	wb = xlrd.open_workbook(filePath)
+	shName = wb.sheet_names()[0]
+	sh = wb.sheet_by_index(0)
+        title = sh.row_values(0)
+        convert_list = []
+        print shName
+        for rownum in range(6, sh.nrows):
+                rowvalue = sh.row_values(rownum)
+                single = OrderedDict()
+                for colnum in range(1, len(rowvalue)):
+                        single[title[colnum]] = rowvalue[colnum]
+                convert_list.append(single)
+        j = json.dumps(convert_list,ensure_ascii=False)
+
+        with codecs.open(shName+'.json',"w","utf-8") as f:
+                f.write(j)
+        
+def checkPathExcel(path, file_types):
+    for file in os.listdir(path):
+        filepath = path + "//"+file 
+        if os.path.isdir(filepath):
+            checkPathExcel(filepath, file_types)
+        elif os.path.isfile(filepath):
+            splitlist = filepath.split('.')
+            m = len(splitlist)
+            prefx = splitlist[m-1]
+            
+            if prefx in file_types:
+                checkExcelFile(filepath,file)
+                
+file_type_list = ["xlsm","xlsx"]
+
+if __name__ == '__main__':
+    checkPathExcel(".", file_type_list)
+    os.system("pause")
     
-j = json.dumps(convert_list,ensure_ascii=False)
 
-with codecs.open('file.json',"w","utf-8") as f:
-    f.write(j)
